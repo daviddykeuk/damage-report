7297d9f21b4710437f30527f889b7c0400022e55
David Dyke <david.dyke@tnsglobal.com>
2018-03-22 13:07:48 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+var p = require('./package.json');
+
+console.info("\n");
+console.info(" _____                                                               _    ");
+console.info("|  __ \\                                                             | |   ");
+console.info("| |  | | __ _ _ __ ___   __ _  __ _  ___   _ __ ___ _ __   ___  _ __| |_  ");
+console.info("| |  | |/ _` | '_ ` _ \\ / _` |/ _` |/ _ \\ | '__/ _ \\ '_ \\ / _ \\| '__| __| ");
+console.info("| |__| | (_| | | | | | | (_| | (_| |  __/ | | |  __/ |_) | (_) | |  | |_  ");
+console.info("|_____/ \\__,_|_| |_| |_|\\__,_|\\__, |\\___| |_|  \\___| .__/ \\___/|_|   \\__| ");
+console.info("                               __/ |               | |");   
+console.info("                              |___/                |_|");      
+
+console.info("Version: %s", p.version);
